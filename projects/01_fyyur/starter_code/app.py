d71c2719baa93c90a8bb3b8df12854bba67a02b9
Rana Saeed <ranas.2122@gmail.com>
2020-07-24 21:31:18 +0200

    Implemented /veneus/search route

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -99,17 +99,15 @@ def venues():
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for Hop should return "The Musical Hop".
-  # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
-  response={
-    "count": 1,
-    "data": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
-  }
-  return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
+  search_term = request.form.get('search_term', '')
+  results = db.session.query(Venue.name, Venue.id).filter(Venue.name.ilike('%' + search_term + '%')).all()
+  
+  class SearchResults(object):
+    def __init__(self, count, venues):
+        self.count = count
+        self.venues = venues
+  searchResults = SearchResults(len(results), results)
+  return render_template('pages/search_venues.html', results=searchResults, search_term=search_term)
 
 @app.route('/venues/<int:venue_id>')
 def show_venue(venue_id):
